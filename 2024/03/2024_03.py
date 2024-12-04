73204a869442d433069e3fef8d9b50a1b71a0e38
Carlos Ortiz <carlos.ortiz@itstoff.se>
2024-12-04 19:10:21 +0100

    Add 2024 03 part1

--- 2024/03/2024_03.py
+++ 2024/03/2024_03.py
@@ -5,12 +5,13 @@ if __name__ == '__main__':
   part1=0
   part2=0
 
-  with (open("2024_03_test.dat", "r") as fp):
+  with (open("2024_03.dat", "r") as fp):
     for line in fp:
-      print(line)
-      print(re.sub(r'[^mul(1-999,1-999)]', ' ', line))
-
+      mul=re.findall(r'(mul\(\d{1,3},\d{1,3}\))', line)
+      for val in mul:
+        v=re.sub(r'[mul()]','',val).split(',')
+        part1+=int(v[0])*int(v[1])
 
   print("--- RESULT ---")
   print(part1)
-  print(part1)
+#  print(part2)
